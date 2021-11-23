a168cfc31736cb8c910dec1ff8cec106f63eba03
Damian Malysza <damian.malysza@gmail.com>
2021-11-22 23:09:26 -0500

    cleaned up serialization for recipe model to only give info needed

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,5 +1,32 @@
 class RecipeSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :preptime, :cooktime, :servings, :recipeingredients, :recipesteps, :category
+  attributes :name, :preptime, :cooktime, :servings, :category
+
+  attributes :ingredients do |recipe|
+    return_array = []
+    recipe.recipeingredients.each do |ingredient|
+      ingredient_hash = {}
+      ingredient_hash[:quantity] = ingredient.quantity
+      ingredient_hash[:unit] = ingredient.unit
+      ingredient_hash[:ingredient] = ingredient.ingredient_name
+      return_array.push(ingredient_hash)
+    end
+    return_array
+  end
+
+  attributes :directions do |recipe|
+    return_array = []
+    recipe.recipesteps.each do |step|
+      step_hash = {}
+      step_hash[:step_number] = step.step_number
+      step_hash[:direction] = step.direction
+      return_array.push(step_hash)
+    end
+    return_array
+  end
+
+  attributes :category do |recipe|
+    recipe.category.name
+  end
 
 end
