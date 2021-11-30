eae276d59efe9be7e513135a4fa135193a2d1125
Damian Malysza <damian.malysza@gmail.com>
2021-11-30 10:36:12 -0500

    changed up seed file to use faker to generate random data for 500 recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Faker::Config.random = nil
+Faker::Config.random.seed
+
 Recipestep.destroy_all
 Recipeingredient.destroy_all
 Ingredient.destroy_all
@@ -22,7 +25,18 @@ beverage = Category.create(name: "Beverage")
 misc = Category.create(name: "Miscellaneous")
 
 #test recipe
-recipe = Recipe.create(name:"scrambled eggs",cooktime:20,category:breakfast)
-ingredient = Ingredient.create(name: "egg")
-quantity = Recipeingredient.create(recipe:recipe,ingredient:ingredient,quantity:3,unit:"eggs")
-step = Recipestep.create(step_number:1,direction:"break some eggs",recipe:recipe)
+500.times do 
+  recipe = Recipe.create(name: Faker::Food.dish, cooktime: rand(1..300), preptime: rand(1..300), servings: rand(1..12),category: Category.all.sample)
+  ingredient = Ingredient.create_or_find_by(name: Faker::Food.ingredient)
+  quantity = Recipeingredient.create(recipe:recipe,ingredient:ingredient,quantity:rand(1..10),unit:Faker::Food.metric_measurement)
+  rand(1..15).times do |count|
+    Recipestep.create(step_number:count,direction:Faker::Lorem.sentence(word_count:rand(5..20)),recipe:recipe)
+  end
+end
+# recipe = Recipe.create(name:"scrambled eggs",cooktime:20,category:breakfast)
+# ingredient = Ingredient.create(name: "egg")
+# quantity = Recipeingredient.create(recipe:recipe,ingredient:ingredient,quantity:3,unit:"eggs")
+# step = Recipestep.create(step_number:1,direction:"break some eggs",recipe:recipe)
+
+
+
