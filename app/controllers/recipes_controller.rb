3232f0f6a5a28ef93444978d8a714a4bd06f0bba
Damian Malysza <damian.malysza@gmail.com>
2021-11-30 17:27:40 -0500

    changed up logic serving recipe data and split out into providing general info vs specific recipe info

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,12 @@
 class RecipesController < ApplicationController
   def index
     recipes = Recipe.all
-    render json: RecipeSerializer.new(recipes)
+    render json: RecipeSerializer.new(recipes, {fields: {recipe: [:name, :preptime, :cooktime, :servings, :category]}})
+  end
+
+  def show
+    recipe = Recipe.find(params[:id])
+    render json: RecipeSerializer.new(recipe)
   end
   
   def create
