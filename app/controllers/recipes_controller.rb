85d3ada319caa0e24172b593b2da17a4d0ca917f
Damian Malysza <damian.malysza@gmail.com>
2021-11-30 15:52:12 -0500

    implemented logic to delete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,17 +3,26 @@ class RecipesController < ApplicationController
     recipes = Recipe.all
     render json: RecipeSerializer.new(recipes)
   end
-
+  
   def create
-
+    
   end
-
+  
   def update
-
+    
   end
-
+  
   def destroy
-
+    recipe = Recipe.find(params[:id])
+    if recipe
+      if recipe.destroy
+        head :no_content
+      else
+        render :json => { :errors => recipe.errors.full_messages }
+      end
+    else
+      render :json => { :errors => recipe.errors.full_messages }
+    end
   end
-
+  
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :category
-  has_many :recipeingredients
+  has_many :recipeingredients, :dependent => :destroy
   has_many :ingredients, through: :recipeingredients
-  has_many :recipesteps
+  has_many :recipesteps, :dependent => :destroy
 end
