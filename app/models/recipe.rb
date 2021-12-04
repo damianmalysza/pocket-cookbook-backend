e629b6c9831dd51b0f7c36923fb3f5cd2e7747f0
Damian Malysza <damian.malysza@gmail.com>
2021-12-04 09:40:50 -0500

    added logic gates to create_from_site_form to produce accurate error on frontend form

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ApplicationRecord
   has_many :recipeingredients, :dependent => :destroy
   has_many :ingredients, through: :recipeingredients
   has_many :recipesteps, :dependent => :destroy
-
+  
   validates :name, presence: true
   validates :preptime, presence: true
   validates :cooktime, presence: true
@@ -11,19 +11,26 @@ class Recipe < ApplicationRecord
   validates :preptime, numericality: {only_integer: true}
   validates :cooktime, numericality: {only_integer: true}
   validates :servings, numericality: {only_integer: true}
-
+  
   def self.create_from_site_form(params)
     category = Category.find_by(name: params[:category])
     recipe = Recipe.new(name: params[:name], cooktime:params[:cooktime], preptime:params[:preptime],servings:params[:servings], category: category)
-    params[:ingredients].each do |ingredient|
-      new_ingredient = Ingredient.find_or_create_by(name:ingredient[:ingredient])
-      # if new_ingredient.save 
-        quantity = Recipeingredient.new(recipe:recipe,ingredient:new_ingredient,quantity:ingredient[:quantity],unit:ingredient[:unit])
-      # else
-        # return new_ingredient
-      # end
+    if recipe.save
+      params[:ingredients].each do |ingredient|
+        new_ingredient = Ingredient.find_or_create_by(name:ingredient[:ingredient])
+        if new_ingredient.save 
+          quantity = Recipeingredient.new(recipe:recipe,ingredient:new_ingredient,quantity:ingredient[:quantity],unit:ingredient[:unit])
+          if !quantity.save
+            return quantity
+          end
+        else
+          return new_ingredient
+        end
+      end
+    else 
+      return recipe
     end
-
+    
     params[:directions].each do |direction|
       Recipestep.create(step_number:direction[:step_number],direction:direction[:direction],recipe:recipe)
     end
